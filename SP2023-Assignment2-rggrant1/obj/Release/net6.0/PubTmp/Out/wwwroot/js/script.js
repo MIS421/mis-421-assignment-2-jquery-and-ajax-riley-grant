e6ca11fcbe7678f60f1a98b019c5af4636bc6b31
riley <rggrant1@crimson.ua.edu>
2023-02-16 18:52:17 -0600

    final

---
+++ SP2023-Assignment2-rggrant1/obj/Release/net6.0/PubTmp/Out/wwwroot/js/script.js
@@ -0,0 +1,36 @@
+var len;
+var results = '';
+
+
+function apiSearch() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0",
+    "mkt": "en-us"
+  };
+
+  $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+      beforeSend: function (xhrObj) {
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "4c5e4f48ac974095bc828a644951581a");
+      },
+      type: "GET",
+    })
+    .done(function (data) {
+      len = data.webPages.value.length;
+      for (i = 0; i < len; i++) {
+        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+        }
+        var Search = document.getElementById("searchResults");
+        Search.style.visibility = "visible";
+
+      $('#searchResults').html(results);
+      $('#searchResults').dialog();
+    })
+    .fail(function () {
+      alert("error");
+    });
+
+
+}
